0fd267805757c166eb5ec6af9dbc9f38f2c3a2a4
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 08:58:47 -0700

    modified the main.html,models.py.created the css folder and a css for the main

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import ndb
 
 class Story(ndb.Model):
-    =ndb.StringProperty(required=True)
+    i=ndb.StringProperty(required=True)
     bottomLine=ndb.StringProperty(required=True)
     picture=ndb.StringProperty(required=True)
