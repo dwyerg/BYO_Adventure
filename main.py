122774100a090909ad03f79621af54b3ddba5cd7
Grace Dwyer <dwyerg@umich.edu>
2019-07-26 16:43:49 -0400

    added read handler

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from google.appengine.ext import ndb
 from models import BYOusers
 from google.appengine.api import urlfetch
 import json
-
-#importing classes from our models sheet
 from models import Story
 from models import StoryPoint
 from models import ChoicePoint
@@ -24,6 +22,8 @@ jinjaEnv = jinja2.Environment(
 )
 
 # the handler section
+
+
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         user= users.get_current_user()
@@ -65,7 +65,6 @@ class MainPage(webapp2.RequestHandler):
 
 
 class OneFormHandler(webapp2.RequestHandler):
-# creating the story as one form with a bunch of textboxes for the whole story
     def get(self):
         oneform_template = jinjaEnv.get_template("oneForm.html")
         self.response.write(oneform_template.render())
@@ -94,17 +93,47 @@ class registrationPage(webapp2.RequestHandler):
         main_template = jinjaEnv.get_template("registration.html")
         self.response.write(main_template.render())
 
+
+# the app configuration section
+
+class CreateHandler(webapp2.RequestHandler):
+    def get(self):
+        title= self.request.get("adventure_name")
+        create_template = jinjaEnv.get_template("create.html")
+        self.response.write(create_template.render())
+
+
+class FirstStepHandler(webapp2.RequestHandler):
+    def post(self):
+        step_template = jinjaEnv.get_template("newStep.html")
+        self.response.write(step_template.render())
+
+        tStory= Story(title= self.request.get("adventure_name"))
+        print("THE TITLE OF THE STORY IS" + tStory.title)
+        tStory.put()
+
+
+class AddForkHandler(webapp2.RequestHandler):
+    def post(self):
+        fork_template = jinjaEnv.get_template("fork.html")
+        self.response.write(fork_template.render())
+        tStoryPoint=StoryPoint(story_key= 8, story_point_id = "ehbeej" , plot_text = self.request.get("story_text_box"))
+        #tStoryPoint=StoryPoint(story_key= "", story_point_id = "" , plot_text = self.request.get("story_text_box"))
+        #story_key=The ID of the story it belongs to, story_point_id is the random thing appengine generates
+        print("HERE IS HOW THE STORY BEGINS:" + "\n" + tStoryPoint.plot_text)
+        tStoryPoint.put()
+
+
+class NewStepHandler(webapp2.RequestHandler):
+    def post(self):
+        fork_template = jinjaEnv.get_template("firstStep.html")
+        self.response.write(fork_template.render())
+
 class ProfileHandler(webapp2.RequestHandler):
     def get(self):
+
         user=users.get_current_user()
-        email_address= user.nickname()
-        existing_user= BYOusers.query().filter(BYOusers.email== email_address).get()
-
-
         logout_dict={
-            "firstname": existing_user.first_name,
-            "lastname": existing_user.last_name,
-            "email": existing_user.email,
             "logout_url": users.create_logout_url('/login')
         }
 
@@ -113,15 +142,13 @@ class ProfileHandler(webapp2.RequestHandler):
 
 
 class ResultHandler(webapp2.RequestHandler):
-#creates the result of whatever the user enters
     def post(self):
+        result_template = jinjaEnv.get_template("result.html")
+        self.response.write(result_template.render())
 
         rStory= Story(title= self.request.get("adventure_name"))
-
-        #shooting the story up to the database
         rStory.put()
 
-        #creating the storypoints based on each text they enter for what happens
         storypoint1 = StoryPoint(story_key = rStory.key, text = self.request.get("first_story_point"))
         storypoint2 = StoryPoint(story_key = rStory.key, text = self.request.get("choiceA_story_point"))
         storypoint3 = StoryPoint(story_key = rStory.key, text = self.request.get("choiceB_story_point"))
@@ -136,7 +163,6 @@ class ResultHandler(webapp2.RequestHandler):
         storypoint12 = StoryPoint(story_key = rStory.key, text = self.request.get("choiceK_story_point"))
         storypoint13 = StoryPoint(story_key = rStory.key, text = self.request.get("choiceL_story_point"))
 
-        #putting all of the storypoints into the database
         storypoint1.put()
         storypoint2.put()
         storypoint3.put()
@@ -151,13 +177,10 @@ class ResultHandler(webapp2.RequestHandler):
         storypoint12.put()
         storypoint13.put()
 
-        #assigning a first id
         rStory.first_story_point_key = storypoint1.key
 
-        #putting / updating the story now that it has a first_id
         rStory.put()
 
-        #creating all of the choice points
         choicepoint1 = ChoicePoint(begin_story_point_key = storypoint1.key, end_story_point_key = storypoint2.key, text = self.request.get("Choice_A"))
         choicepoint2 = ChoicePoint(begin_story_point_key = storypoint1.key, end_story_point_key = storypoint3.key, text = self.request.get("Choice_B"))
         choicepoint3 = ChoicePoint(begin_story_point_key = storypoint2.key, end_story_point_key = storypoint4.key, text = self.request.get("Choice_C"))
@@ -171,10 +194,8 @@ class ResultHandler(webapp2.RequestHandler):
         choicepoint11 = ChoicePoint(begin_story_point_key = storypoint7.key, end_story_point_key = storypoint12.key, text = self.request.get("Choice_K"))
         choicepoint12 = ChoicePoint(begin_story_point_key = storypoint7.key, end_story_point_key = storypoint13.key, text = self.request.get("Choice_L"))
 
-        #putting the story again
         rStory.put()
 
-        #putting the storypoints now that they have corresponding choicepoints
         storypoint1.put()
         storypoint2.put()
         storypoint3.put()
@@ -189,8 +210,6 @@ class ResultHandler(webapp2.RequestHandler):
         storypoint12.put()
         storypoint13.put()
 
-
-        #putting the choicepoints
         choicepoint1.put()
         choicepoint2.put()
         choicepoint3.put()
@@ -204,50 +223,6 @@ class ResultHandler(webapp2.RequestHandler):
         choicepoint11.put()
         choicepoint12.put()
 
-        display_dictionary = {
-
-            "display_title": rStory.title,
-            "display_sp_1" : storypoint1.text,
-            "display_sp_2" : storypoint2.text,
-            "display_sp_3" : storypoint3.text,
-            "display_sp_4" : storypoint4.text,
-            "display_sp_5" : storypoint5.text,
-            "display_sp_6" : storypoint6.text,
-            "display_sp_7" : storypoint7.text,
-            "display_sp_8" : storypoint8.text,
-            "display_sp_9" : storypoint9.text,
-            "display_sp_10" : storypoint10.text,
-            "display_sp_11" : storypoint11.text,
-            "display_sp_12" : storypoint12.text,
-            "display_sp_13" : storypoint13.text,
-
-            "display_cp_1" : choicepoint1.text,
-            "display_cp_2" : choicepoint2.text,
-            "display_cp_3" : choicepoint3.text,
-            "display_cp_4" : choicepoint4.text,
-            "display_cp_5" : choicepoint5.text,
-            "display_cp_6" : choicepoint6.text,
-            "display_cp_7" : choicepoint7.text,
-            "display_cp_8" : choicepoint8.text,
-            "display_cp_9" : choicepoint9.text,
-            "display_cp_10": choicepoint10.text,
-            "display_cp_11": choicepoint11.text,
-            "display_cp_12": choicepoint12.text,
-
-        }
-
-        result_template = jinjaEnv.get_template("result.html")
-        #self.response.write(result_template.render())
-        self.response.write(result_template.render(display_dictionary))
-        print("displaying story!" + display_dictionary["display_sp_1"])
-        #creating the story for what the user will input
-        #with only a title for now bc we will make the first id later
-
-    def get(self):
-        
-        pass
-
-
 class BrowseHandler(webapp2.RequestHandler):
     def get(self):
         allStories = Story.query().fetch()
@@ -255,34 +230,31 @@ class BrowseHandler(webapp2.RequestHandler):
         allChoicePoints = ChoicePoint.query().fetch()
         all_dict = {
             "theStories" : allStories,
+            #"theStoryPoints" : allStoryPoints,
+            #"theChoicePoints" : allChoicePoints
         }
         all_stories_template = jinjaEnv.get_template("browse.html")
         self.response.write(all_stories_template.render(all_dict))
 
 class ReadHandler(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         read_template = jinjaEnv.get_template("read.html")
         self.response.write(read_template.render())
-class StoryPointAPI(webapp2.RequestHandler):
-    def get(self):
-        id = self.request.get("id")
-        simple_dict = {
-            "story_text": ,
-            "pear": "orange"
-        }
-        self.response.write(json.dumps(simple_dict))
 
 app = webapp2.WSGIApplication(
     [
         ("/", MainPage),
         ('/login', LoginHandler),
         ("/registration",registrationPage),
+        ('/create', CreateHandler),
+        ('/firstStep', FirstStepHandler),
+        ('/newStep', NewStepHandler),
+        ('/addFork', AddForkHandler),
         ('/profile',ProfileHandler),
         ('/oneform', OneFormHandler),
         ('/result', ResultHandler),
         ('/browse', BrowseHandler),
-        ('/read', ReadHandler),
-        ('/api/story', StoryPointAPI)
+        ('/read', ReadHandler)
     ],
     debug=True
     )
